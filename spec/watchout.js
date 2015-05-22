08e800d2066c314837624e3a89d63c459f114ce4
Ronnie Brown <ronnievoice@gmail.com>
2015-05-22 11:39:12 -0700

    add test suite

---
+++ spec/watchout.js
@@ -0,0 +1,20 @@
+/* global watchout, describe, it, expect, should */
+
+describe('watchout()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(watchout).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
