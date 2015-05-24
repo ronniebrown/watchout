42eec2dab9591ca5bb8e15c636e4de02d358affd
Ronnie Brown <ronnievoice@gmail.com>
2015-05-23 17:40:27 -0700

    core requirements complete

--- watchout.js
+++ watchout.js
@@ -1,121 +1,122 @@
-var width = 900;
+// start slingin' some d3 here.
 var height = 600;
-var numAsteroids = 50;
+var width = 1000;
+var nEnemies = 15;
 
 var axes = {
   x: d3.scale.linear().domain([0, 100]).range([0, width]),
   y: d3.scale.linear().domain([0, 100]).range([0, height])
 };
 
-var gameboard = d3.select("body").selectAll(".scoreboard").append("svg")
-                                 .attr("width", width)
-                                 .attr("height", height)
-                                 .style("border", "1px solid black")
-                                 .classed("gameboard", true);
+var gameBoard = d3.select('.board').append('svg:svg')
+  .attr('width', width)
+  .attr('height', height)
+  .classed('gameboard', true);
 
-//  set enemies
-var createAsteroids = function(n){
+var drag = d3.behavior.drag()  
+             .on('dragstart', function() { player.style('opacity', '0.4'); })
+             .on('drag', function() { player.attr('cx', d3.event.x)
+                                            .attr('cy', d3.event.y); })
+             .on('dragend', function() { player.style('opacity', '1.0'); });
 
-  var asteroids = [];
-  for(var i = 0; i < n; i++){
-    asteroids.push({
+var player = gameBoard.selectAll('.draggableplayer')  
+                .data([{ x: (width / 2), y: (height / 2), r: 12 }])
+                .enter()
+                .append('svg:circle')
+                .attr('class', 'draggableplayer')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                .call(drag)
+                .style('fill', 'green');
+
+var createEnemies = function(n) {
+  var enemies = [];
+  for (var i = 0; i < n; i++) {
+    enemies.push({
       id: i,
-      x: Math.random()*100,
-      y: Math.random()*100
+      x: Math.random() * 100,
+      y: Math.random() * 100
     });
   }
-  return asteroids;
+  return enemies;
 };
 
-//  render the gameboard
-var boardRenderer = function(a){
-  var asteroid = gameboard.selectAll('circle.asteroid').data(a, function(e){
-    return e.id;
+var render = function(enemyData) {
+  var enemies = gameBoard.selectAll('circle.enemy').data(enemyData, function(d) {
+    return d.id;
   });
 
-  asteroid.enter()
+  enemies.enter()
     .append('svg:circle')
-      .attr('cx', function(asteroid){
-        return axes.x(asteroid.x);
-      })
-      .attr('cy', function(asteroid) {
-        return axes.y(asteroid.y)
-      })
-      .attr('r', 25)
-      .attr('fill', 'green')
-      .attr('stroke', 'blue');
+    .attr('class', 'enemy')
+    .attr('cx', function(enemy) {
+      return axes.x(enemy.x);
+    })
+    .attr('cy', function(enemy) {
+      return axes.y(enemy.y);
+    })
+    .attr('r', 15)
+    .attr('fill', 'white')
+    .attr('stroke', 'blue');
 
-  asteroid.exit().remove();
+  enemies.exit().remove();
 
-  var checkCollision = function(asteroid, collidedCallback){
-    _.each(asteroids, function(asteroid){
-      radiusSum =  parseFloat(asteroid.attr('r')) + player.r;
-      xDiff = parseFloat(asteroid.attr('cx')) - player.x;
-      yDiff = parseFloat(asteroid.attr('cy')) - player.y;
-
-      separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) )
-      if (separation < radiusSum){
-        collidedCallback(player, asteroid);
-      }
-    });
-      
-  }
-
-  var Player = function () {
-    gameboard.append('svg:circle')
-      .attr('cx', board.width / 2)
-      .attr('cy', board.height / 2)
-      .attr('r', 17)
-      .attr('fill', 'red')
-      .call(d3.behavior.drag()
-        .on('drag', function(d) {
-          moveObject.call(this);
-    }));
+  var checkCollision = function(enemy, player, collidedCallback) {
+  
+    var radiusSum, separation, xDiff, yDiff;
+    _.each(player, function() {
+    radiusSum = parseFloat(enemy.attr('r')) + player.r;
+    xDiff = parseFloat(enemy.attr('cx')) - player.x;
+    yDiff = parseFloat(enemy.attr('cy')) - player.y;
+    separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
+    if (separation < radiusSum) {
+      return collidedCallback(player, enemy);
+    };
+  });
   };
 
-  var tweenWithCollisionDetection = function(endData){
-    var endPosition, asteroidNextPosition, startPosition;
+  var onCollision = function(){
+    numenemies = 1;
+    enemies.transition()
+      .attr('oppacity', '0.4')
+      .transition()
+      .attr('oppacity', '1');
+  }
 
-    asteroid = d3.select(this);
-
-    startPosition = {
-      x: parseFloat(asteroid.attr('cx')),
-      y: parseFloat(asteroid.attr('cy'))
+  var tweenWithCollisionDetection = function(endData) {
+    var endPos, enemy, startPos;
+    enemy = d3.select(this);
+    startPos = {
+      x: parseFloat(enemy.attr('cx')),
+      y: parseFloat(enemy.attr('cy'))
     };
-
-    endPosition = {
+    endPos = {
       x: axes.x(endData.x),
       y: axes.y(endData.y)
     };
-
     return function(t) {
-      return checkCollision(asteroid);
-    
-      var asteroidNextPosition;
-      asteroidNextPosition = {
+      checkCollision(enemy, onCollision);
+      var enemyNextPos;
+      enemyNextPos = {
         x: startPos.x + (endPos.x - startPos.x) * t,
         y: startPos.y + (endPos.y - startPos.y) * t
       };
-      return asteroid.attr('cx', asteroidNextPosition.x).attr('cy', asteroidNextPosition.y);
+      enemy.attr('cx', enemyNextPos.x).attr('cy', enemyNextPos.y);
     };
   };
-  return asteroid.transition().duration(500).attr('r', 25).transition().duration(2000).tween('custom', tweenWithCollisionDetection);  
-  asteroid.exit().remove();
+  enemies.transition().duration(300).attr('r', 15).transition().duration(800).tween('custom', tweenWithCollisionDetection);
 };
 
-//  play game woot woot
 play = function() {
   var turn;
   turn = function() {
-    var newAsteroidPositions = createAsteroids(numAsteroids);
-    return boardRenderer(newAsteroidPositions);
+    var newEnemyPositions = createEnemies(nEnemies);
+    render(newEnemyPositions);
   };
 
   turn();
-  // return setInterval(turn, 1000);
+  setInterval(turn, 1000);
 };
 
 play();
-
-
-
